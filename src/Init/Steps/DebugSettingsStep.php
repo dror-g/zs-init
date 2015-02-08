d990addfc5b46ec97b8e21ac8851500578deed1b
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2015-02-08 15:15:41 +0200

    Fix debug settings regexps ZCLOUD-204

--- src/Init/Steps/DebugSettingsStep.php
+++ src/Init/Steps/DebugSettingsStep.php
@@ -17,10 +17,10 @@ class DebugSettingsStep extends AbstractStep
         $state->log->log(Log::INFO,"Starting {$this->name}");
         if(isset($state["ZEND_DEBUG"]) && $state["ZEND_DEBUG"] === true) {
             $state->log->log(Log::INFO,"Setting logging level to debug in ini files");
-            self::pregReplaceFile("/zend_gui.logVerbosity = NOTICE/", "zend_gui.logVerbosity = DEBUG", "/usr/local/zend/gui/config/zs_ui.ini");
-            self::pregReplaceFile('/zend_gui.debugModeEnabled = false/', 'zend_gui.debugModeEnabled = true', "/usr/local/zend/gui/config/zs_ui.ini");
-            self::pregReplaceFile('/zend_deployment.daemon.log_verbosity_level=2/', 'zend_deployment.daemon.log_verbosity_level=5', "/usr/local/zend/etc/zdd.ini");
-            self::pregReplaceFile('/zend_server_daemon.log_verbosity_level=2/', 'zend_server_daemon.log_verbosity_level=5', "/usr/local/zend/etc/zsd.ini");
+            self::pregReplaceFile('/zend_gui.logVerbosity\\s*=.*$/m', "zend_gui.logVerbosity = DEBUG", "/usr/local/zend/gui/config/zs_ui.ini");
+            self::pregReplaceFile('/zend_gui.debugModeEnabled\\s*=.*$/m', 'zend_gui.debugModeEnabled = true', "/usr/local/zend/gui/config/zs_ui.ini");
+            self::pregReplaceFile('/zend_deployment.daemon.log_verbosity_level\\s*=.*$/m', 'zend_deployment.daemon.log_verbosity_level=5', "/usr/local/zend/etc/zdd.ini");
+            self::pregReplaceFile('/zend_server_daemon.log_verbosity_level\\s*=.*$/m', 'zend_server_daemon.log_verbosity_level=5', "/usr/local/zend/etc/zsd.ini");
         }
 
         $state->log->log(Log::INFO,"Cleaning semaphores");
