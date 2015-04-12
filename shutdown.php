455487dd875293dd333f83d5c420a50f3c2edd94
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2015-04-12 12:50:02 +0300

    Use clusterRemoveServer instead of clusterForceRemoveServer ZAWS-44

--- shutdown.php
+++ shutdown.php
@@ -15,9 +15,9 @@ $state = new State($log);
 if(isset($state['WEB_API_KEY_NAME'],$state['WEB_API_KEY_HASH'],$state['NODE_ID'])) {
     $log->log(Log::INFO, "Removing server from cluster");
     $client = new ZSWebApiClient($state['WEB_API_KEY_NAME'],$state['WEB_API_KEY_HASH']);
-    $result = $client->clusterForceRemoveServer(['serverId' => $state['NODE_ID']]);
-
+    $result = $client->clusterRemoveServer(['serverId' => $state['NODE_ID']]);
     $log->log(Log::INFO, "Zend Server remove from cluster WebAPI response:\nHTTP Response code: {$result['code']}\nReason: {$result['reason']}\n");
+    unset($state['NODE_ID']);
 }
 
 exit(0);
