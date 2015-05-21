0b5173340786b075bd0d2b02fcb531134330324a
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2015-05-21 13:06:13 +0300

    Generate warning when no license was found and continue with empty license

--- src/Init/Steps/LicenseStep.php
+++ src/Init/Steps/LicenseStep.php
@@ -22,19 +22,24 @@ class LicenseStep extends AbstractStep
             $state->log->log(Log::INFO, "Analyzing license file");
             $license = json_decode(file_get_contents(self::LICENSE_FILE), true);
             if ($license == null) {
-                return new Result(Result::STATUS_SUCCESS, "Could not parse license file");
+                $state->log->log(Log::WARNING, "Could not parse license file");
+                $state['ZEND_LICENSE_KEY'] = "";
+                $state['ZEND_LICENSE_ORDER'] = "";
+            } else {
+                $state->log->log(Log::INFO, "Setting up license");
+                $state['ZEND_LICENSE_KEY'] = $license['ZEND_LICENSE_KEY'];
+                $state['ZEND_LICENSE_ORDER'] = $license['ZEND_LICENSE_ORDER'];
             }
 
-            $state->log->log(Log::INFO, "Setting up license");
-            $state['ZEND_LICENSE_KEY'] = $license['ZEND_LICENSE_KEY'];
-            $state['ZEND_LICENSE_ORDER'] = $license['ZEND_LICENSE_ORDER'];
-            self::pregReplaceFile("/zend\\.serial_number=.*\$/", "zend.serial_number={$license['ZEND_LICENSE_KEY']}", "/usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini");
-            self::pregReplaceFile("/zend\\.user_name=.*\$/", "zend.user_name={$license['ZEND_LICENSE_ORDER']}", "/usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini");
-            exec("sqlite3 /usr/local/zend/var/db/zsd.db \"UPDATE ZSD_DIRECTIVES set DISK_VALUE='{$license['ZEND_LICENSE_KEY']}' WHERE NAME='zend.serial_number'\"");
-            exec("sqlite3 /usr/local/zend/var/db/zsd.db \"UPDATE ZSD_DIRECTIVES set DISK_VALUE='{$license['ZEND_LICENSE_ORDER']}' WHERE NAME='zend.user_name'\"");
+            self::pregReplaceFile("/zend\\.serial_number=.*\$/", "zend.serial_number={$state['ZEND_LICENSE_KEY']}", "/usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini");
+            self::pregReplaceFile("/zend\\.user_name=.*\$/", "zend.user_name={$state['ZEND_LICENSE_ORDER']}", "/usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini");
+            exec("sqlite3 /usr/local/zend/var/db/zsd.db \"UPDATE ZSD_DIRECTIVES set DISK_VALUE='{$state['ZEND_LICENSE_KEY']}' WHERE NAME='zend.serial_number'\"");
+            exec("sqlite3 /usr/local/zend/var/db/zsd.db \"UPDATE ZSD_DIRECTIVES set DISK_VALUE='{$state['ZEND_LICENSE_ORDER']}' WHERE NAME='zend.user_name'\"");
 
-            $state->log->log(Log::INFO, "Deleting license file");
-            unlink(self::LICENSE_FILE);
+            if (is_file(self::LICENSE_FILE)) {
+                $state->log->log(Log::INFO, "Deleting license file");
+                unlink(self::LICENSE_FILE);
+            }
         }
         $state->log->log(Log::INFO, "Finished {$this->name}");
         return new Result(Result::STATUS_SUCCESS);
